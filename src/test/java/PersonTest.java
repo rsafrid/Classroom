3defc77d63deca7a29c976c28a8605d6d9bda9c2
Susanne Fridh <fridh.susanne@gmail.com>
2017-11-24 14:16:56 +0100

    second commit. Not 100% yet.

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -5,11 +5,29 @@ import org.junit.Test;
 import otherclasses.Person;
 
 public class PersonTest {
+	Person p = new Person("Rafael", "Silva", 30,'M');
 
 	@Test
-	public void test() {
-		Person p = new Person("Rafael", "Silva", 30,'M');
-		assertEquals(p.getFirstName().equals("Rafael"),true);
+	public void testSetAndGetFirstName() {
+		p.setFirstName("Rafael2");
+		assertEquals(p.getFirstName().equals("Rafael2"),true);
 	}
 
+	@Test
+	public void testSetAndGetLastName() {
+		p.setLastName("Silva2");
+		assertEquals(p.getLastName().equals("Silva2"),true);
+	}
+	
+	@Test
+	public void testSetAndGetAge() {
+		p.setAge(25);
+		assertEquals(p.getAge()== 25,true);
+	}
+	
+	@Test
+	public void testSetAndGetGender() {
+		p.setGender('M');
+		assertEquals(p.getGender().equals('M'),true);
+	}
 }
